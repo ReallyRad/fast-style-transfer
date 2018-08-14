598f98bbd10a939f7bc36528ec177bea8ed338de
ReallyRad <arthur.pointeau@gmail.com>
2018-08-15 00:07:53 +0200

    preparing style training with medusa

--- setup.sh
+++ setup.sh
@@ -6,3 +6,9 @@ wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 mkdir bin
 wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
 unzip train2014.zip
+
+python style.py --style style/medusas-mas-venenosas.jpg \
+  --checkpoint-dir . \
+  --content-weight 1.5e1 \
+  --checkpoint-iterations 1000 \
+  --batch-size 20
